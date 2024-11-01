a0766bd0bd1d326d8a31be3f1b55f09d6b2860df
heloowird <heloowird@gmail.com>
2024-11-01 23:39:47 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #author: heloowird
+#
+set -eo pipefail
 
 echo "# step1: backup the old config"
 backup_timestamp=$(date +%s)
@@ -12,8 +14,16 @@ then
     cp -r ~/.vim ~/.vim.bak.${backup_timestamp}
 fi
 
+
 echo "# step2: copy the new config"
 cp -f vimrc ~/.vimrc && cp -r vim ~/.vim
+
+echo "# step3: clone Vundle to install plugin"
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "# step4: install vim plugin"
+vim +PluginInstall +qall
+
 if [ $? -eq 0 ]
 then
     echo ' configure vim successfully!'
